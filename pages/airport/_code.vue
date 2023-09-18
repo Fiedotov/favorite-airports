d172613d5e04a7bb32aa42e2895ee6153d7c01ce
Fiedotov <ihorfiedotov@gmail.com>
2023-09-18 16:15:07 +0300

    complete app

---
+++ pages/airport/_code.vue
@@ -0,0 +1,27 @@
+<template></template>
+<script>
+import airports from '~/data/airports.js'
+
+export default {
+    name: 'AirportDetailPage',
+    asyncData(context) {
+        const airport = airports.filter(airport => airport.abbreviation === context.route.params.code.toUpperCase())[0]
+        return {
+            airport
+        }
+    },
+    head() {
+        return {
+            title: 'Airport Information | Aiport App',
+            meta: [
+                {
+                    hid: 'description',
+                    name: 'description',
+                    content: 'Detailed information about the specific airport.'
+                }
+            ]
+        }
+    },
+    layout: 'AirportDetail'
+}
+</script>
